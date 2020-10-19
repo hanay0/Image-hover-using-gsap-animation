044b9b0a7d43857cc3fe018f2244efc09d6916d6
hanafy0 <30327222+hanay0@users.noreply.github.com>
2020-10-19 15:42:14 +0200

    refactoring code
    - change images
    - setting the properties of distortion effect

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 new hoverEffect({
-    parent:document.querySelector('.destortion'),
-    intensity: 0.2,
-    image1: './img1.jpg',
-    image2: './img2.jpg',
-    displacementImage: './stripe1.png'
+    parent:document.querySelector('.distortion'),
+    intensity: 1,
+    image1: './Img13.jpg',
+    image2: './Img14.jpg',
+    angle: Math.PI / 4,
+    speedIn:1.3,
+    speedOut: .9,
+    displacementImage: './patt3.jpg'
 });
