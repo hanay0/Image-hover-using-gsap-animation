dbe50afa57195a3187bfad1ca9acbc035ee86cbd
hanafy0 <30327222+hanay0@users.noreply.github.com>
2020-10-19 14:30:43 +0200

    getting starter files to work with
    - get the link of gsap library
    - get the link of three.js library
    - get hover effect image
    - get 2 images to work with

---
+++ main.js
@@ -0,0 +1,7 @@
+new hoverEffect({
+    parent:document.querySelector('.destortion'),
+    intensity: 0.2,
+    image1: './img1.jpg',
+    image2: './img2.jpg',
+    displacementImage: './stripe1.png'
+});
